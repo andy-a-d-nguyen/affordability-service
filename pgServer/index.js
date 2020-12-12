f636c5c72568fa10b3d48906201100d3a7117656
andy-a-d-nguyen <a.d.nguyen.andy@gmail.com>
2020-12-12 11:52:35 -0800

    modified routes

--- pgServer/index.js
+++ pgServer/index.js
@@ -7,11 +7,15 @@ const PORT = 8020;
 
 app.use(express.json());
 
-app.use('/mortgage/:id', express.static('client/dist'));
+// app.use('/mortgage/:id', express.static('client/dist'));
+app.use('/listings/:id/mortgage', express.static('client/dist'));
 
 app.listen(PORT, () => {
   console.log(`Listening on http://localhost:${PORT}`);
 });
 
-app.get('*/:id/get', pgController.get);
-app.post('*/:id/post', pgController.post);
+// app.get('*/:id/get', pgController.get);
+// app.post('*/:id/post', pgController.post);
+
+app.get('/listings/:id/mortgage/get', pgController.get);
+app.post('/listings/:id/mortgage/post', pgController.post);
